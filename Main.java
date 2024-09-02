d86cb49013f8fd00df4e1cad68472d7d636b889f
MiguelAraujoDeSouza <killerclow2812@gmail.com>
2024-09-02 19:32:03 -0300

    ..

--- Main.java
+++ Main.java
@@ -3,14 +3,14 @@ import java.sql.Date;
 public class Main {
     public static void main(String[] args) {
         Conexao conexao = new Conexao();
-        //Teste de conexão
-//        if(conexao.conectar()){
-//            System.out.println("FUNFO DIMAISI");
-//        }else{
-//            System.out.println("num funfo :(");
-//        }
-        Usuario usuario1 = new Usuario();
-        usuario1.inserirUsuario("x","x", Date.valueOf("2004-12-29"),"x","x","x","x","xx");
+//        Teste de conexão
+        if(conexao.conectar()){
+            System.out.println("FUNFO DIMAISI");
+        }else{
+            System.out.println("num funfo :(");
+        }
+//        Usuario usuario1 = new Usuario();
+//        usuario1.inserirUsuario("x","x", Date.valueOf("2004-12-29"),"x","x","x","x","xx");
     }
 
 }
