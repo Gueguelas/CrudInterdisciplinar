e3a2b45090ebcf90c50bae0ccea1e1010f1b6c06
MiguelAraujoDeSouza <killerclow2812@gmail.com>
2024-10-04 11:43:34 -0300

    Atualizando a main com .env na conexão do banco

--- Conexao.java
+++ Conexao.java
@@ -1,23 +1,27 @@
+import java.sql.Connection; // Mantém a conexão aberta e fecha
+import java.sql.DriverManager; // Cria uma conexão
+import java.sql.SQLException; // Exceções
 
-import java.sql.Connection; //Mantêm a conexão aberta e fecha
-import java.sql.DriverManager; //Cria uma conexão
-import java.sql.SQLException; //Exceções
 public class Conexao {
     private Connection conn;
+    String nomeUsusario = System.getenv("NOME_USUARIO_DO_BANCO_DE_DADOS");
+    String senhaBanco = System.getenv("SENHA_DO_BANCO_DE_DADOS");
+    String maintanceBanco = System.getenv("MAINTANCE_DATABASE_BANCO_DE_DADOS");
+    String portaBanco = System.getenv("PORTA_DO_BANCO_DE_DADOS");
+    String hostBanco = System.getenv("HOST_BANCO_DE_DADOS");
 
     public Connection getConn() {
         return conn;
     }
 
-    //Metodo de conexão com bando de dados
+    // Método de conexão com banco de dados
     public boolean conectar() {
         try {
             Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection(
-                    "jdbc:postgresql://dbbimo-dbbimo.b.aivencloud.com:27539/defaultdb", "avnadmin", "AVNS_D2wIoNxVHuwNinxXXOT");
+            conn = DriverManager.getConnection("jdbc:postgresql://" + hostBanco + ":" + portaBanco + "/" + maintanceBanco, nomeUsusario, senhaBanco);
             return true;
         }
-        //Tratamento de excessão
+        // Tratamento de exceção
         catch (ClassNotFoundException cnfe) {
             cnfe.printStackTrace();
         }
@@ -27,15 +31,15 @@ public class Conexao {
         return false;
     }
 
-    //Desconectar
+    // Desconectar
     public void desconectar() {
         try {
             if (conn != null && !conn.isClosed()) {
-                //Desconectando do BD
+                // Desconectando do BD
                 conn.close();
             }
         }
-        //Tratamento de excessão
+        // Tratamento de exceção
         catch (SQLException sqle) {
             sqle.printStackTrace();
         }
