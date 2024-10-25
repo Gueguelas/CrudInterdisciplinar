473a2c8d6b98e6f8856a2c529fe633a4c4960623
Dora3003 <153046278+Dora3003@users.noreply.github.com>
2024-10-25 07:00:09 -0300

    Subindo para Mari

--- Modelos/Midia.java
+++ Modelos/Midia.java
@@ -1,8 +1,16 @@
 package Modelos;
 
 public class Midia {
+    private int sId;
     private String cURLVideo;
     private String cURLFoto;
+    private boolean bis_inactive;     // Indica se a categoria está inativa
+    private boolean bis_updated;       // Indica se a categoria foi atualizada
+    private boolean btransaction_made;
+
+    public Midia(int sId) { // Construtor para remoção
+        this.sId = sId;
+    }
 
     public String getcURLVideo() {
         return cURLVideo;
